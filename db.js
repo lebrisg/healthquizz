614d945bd441b00c8a4423c53bda3ba2a3bbc50a
Gilles Le Bris <lebrisg@hotmail.com>
2021-09-16 17:16:57 +0200

    Test db.js

--- db.js
+++ db.js
@@ -9,29 +9,36 @@ function init() {
     console.log("Connected successfully to server at:", config.mongoURL);
     var nbDocs = conn.collection('healthdata').count();
     console.log('nbDocs:', nbDocs);
+   }).catch((err) => {
+    console.log(err);
+   }).finally(() => {
+    conn.close();
+   });
 
-    // If no document in the database
-    if (nbDocs == 0) {
-      // Read the file healthdata
-      const data = fs.readFileSync('/mnt/healthdata', 'utf8');
-      console.log(data);
+  // If no document in the database
+  if (nbDocs == 0) {
+    // Read the file healthdata
+    const data = fs.readFileSync('/mnt/healthdata', 'utf8');
+    console.log(data);
 
-      // Transform it into Json
-      const docs = JSON.parse(data.toString());
+    // Transform it into Json
+    const docs = JSON.parse(data.toString());
 
+    client.connect(config.mongoURL, (err, conn) => {
+      if (err) throw err;
       // Insert it into the database
       conn.collection('healthdata')
         .insertMany(docs, function(err, result) {
           if (err) throw err;
           console.log('Inserted docs:', result.insertedCount);
        });
-     }
-   }).catch((err) => {
-    console.log(err);
-   }).finally(() => {
-    conn.close();
-   });
- }
+     }).catch((err) => {
+      console.log(err);
+     }).finally(() => {
+      conn.close();
+     });
+   }
+}
 
 exports.init = init
 
