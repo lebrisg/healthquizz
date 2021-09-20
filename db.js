2faf870fc5da7d9808463c835acf824427a87004
Gilles Le Bris <lebrisg@hotmail.com>
2021-09-20 17:47:13 +0200

    Test db.js

--- db.js
+++ db.js
@@ -8,8 +8,8 @@ async function init() {
   await client.connect();
   console.log("Connected successfully to server at:", config.mongoURL);
   const db = client.db(config.mongoDatabase);
-  nbDocs = await db.collection("healthdata").count();
-    console.log('nbDocs:', nbDocs);
+  nbDocs = await db.collection('healthdata').count();
+  console.log('nbDocs:', nbDocs);
 
   // If no document in the database
   if (nbDocs == 0) {
@@ -20,30 +20,28 @@ async function init() {
     // Transform it into Json
     const docs = JSON.parse(data.toString());
 
-//    MongoClient.connect(config.mongoURL, function(err, db) {
-//      if (err) throw err;
-      // Insert it into the database
-      db.collection('healthdata')
-        .insertMany(docs, function(err, result) {
-          if (err) throw err;
-          console.log('Inserted docs:', result.insertedCount);
-       });
+    // Insert it into the database
+    db.collection('healthdata')
+      .insertMany(docs, function(err, result) {
+        if (err) throw err;
+        console.log('Inserted docs:', result.insertedCount);
+     });
    }
   client.close();
 }
 
 exports.init = init
 
-function getAll() {
-  MongoClient.connect(config.mongoURL, function(err, db) {
-    if (err) throw err;
-    db.collection('healthdata')
-      .find({}).toArray().then(result => {
-        console.log('=>Docs:', result);
-        db.close();
-        return result;
+async function getAll() {
+  const client = new MongoClient(config.mongoURL);
+  await client.connect();
+  const db = client.db(config.mongoDatabase);
+  await db.collection('healthdata')
+    .find({}).toArray().then(result => {
+      console.log('=>Docs:', result);
+      client.close();
+      return result;
      });
-   });
 }
 
 exports.getAll = getAll
