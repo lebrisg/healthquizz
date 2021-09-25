787cc8bdce7a859e544ac7bc81ec8cd4ed10c96a
Gilles Le Bris <lebrisg@hotmail.com>
2021-09-25 12:22:41 +0200

    Test db.js

--- db.js
+++ db.js
@@ -50,7 +50,7 @@ async function getOneItem(callback) {
   const client = new MongoClient(config.mongoURL);
   await client.connect();
   const db = client.db(config.mongoDatabase);
-  await db.healthdata.aggregate([{$sample: {size: 1}}])
+  await db.getCollection('healthdata').aggregate([{$sample: {size: 1}}])
     .toArray().then(result => {
       console.log(result);
       client.close();
