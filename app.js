990e0afc207eae631123d0d6ca0051ced95e5fba
Gilles Le Bris <lebrisg@hotmail.com>
2021-09-26 15:41:25 +0200

    Test app.js

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ collectDefaultMetrics({ prefix: 'healthquizz:' });
 
 // Define render engine used
 //app.engine('html', ejs.renderFile);
-//app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.resolve(__dirname, "views"));
+//app.set("views", "./views");
 
 // Define health checks
 app.get("/ready", (req, res) => res.status(200).json({ status: "ok" }));
