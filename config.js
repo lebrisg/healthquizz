3b2679a05c1e7e3b62bec76637d633079d2343ff
Gilles Le Bris <lebrisg@hotmail.com>
2021-12-13 16:37:47 +0100

    Change HOST & PORT variables.

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-var mongoHost = process.env['MONGODB_SERVICE_HOST'];
-var mongoPort = process.env['MONGODB_SERVICE_PORT'];
+var mongoHost = process.env['MONGO_SERVICE_HOST'];
+var mongoPort = process.env['MONGO_SERVICE_PORT'];
 var mongoDatabase = process.env['MONGODB_DATABASE'];
 var mongoPassword = process.env['MONGODB_PASSWORD'];
-var mongoUser = process.env['MONGODB_USER'];
+var mongoUser = process.env['MONGODB_USERNAME'];
 var mongoURL = "mongodb://" + mongoUser + ":" + mongoPassword + "@mongodb:" + mongoPort + "/" + mongoDatabase;
 
 function display() {
