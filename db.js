6351e8ab61226972204ddde62c9902480d3f2883
Gilles Le Bris <lebrisg@hotmail.com>
2021-12-14 17:28:28 +0100

    Test db.js

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ async function init() {
   console.log("Connected successfully to server at:", config.mongoURL);
   const db = client.db(config.mongoDatabase);
   nbDocs = await db.collection('healthdata').count();
-  //console.log("NbDocs:", nbDocs);
+  console.log("NbDocs:", nbDocs);
   client.close();
 
   // If no document in the database
